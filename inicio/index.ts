8d79ef55b198038c7c2de9d5f1f6c9b33426dc3f
Eduvieira-1 <edu.vieira1@icloud.com>
2024-03-03 13:17:04 -0300

    types

---
+++ inicio/index.ts
@@ -0,0 +1,6 @@
+
+const pedido = ((pedido: any) => {
+    return `Pedido realizado com sucesso ${pedido}`;
+})
+
+console.log(pedido(150));
---
+++ tipos/index.ts
@@ -0,0 +1,14 @@
+let pedido: string = "Pizza";
+pedido = "Eduardo cesar";
+
+let numero: number = 123;
+numero = 321;
+
+let sucesso: boolean = false;
+sucesso = true;
+
+const fun = ((value: string): string => {
+    return value
+})
+
+console.log(fun("Eduardo cesar"));
