548e3ea52b7f9dcd0b2bb1cb018941bd641af3e1
Eduvieira-1 <edu.vieira1@icloud.com>
2024-03-03 18:45:37 -0300

    static and readonly

--- classes/herança/index.ts
+++ classes/herança/index.ts
@@ -1,4 +1,4 @@
-class Pessoa{
+abstract class Pessoa{
     public nome: string = "";
     public idade: number = 0;
 
@@ -15,32 +15,22 @@ class Pessoa{
     fezNiver(){
         return `O ${this.nome} fez tantos anos ${++this.idade}`;
     }
+
+    protected abstract profissao: string;
+    public abstract qualSuaProfissao(): string;
 }
 
 class Edu extends Pessoa{
-    private _profissao: string = "programador"; 
+    protected profissao: string = "programador"; 
     constructor(nome: string, idade: number){
         super(nome, idade);
     }
 
-    get profissao(){
-        return this._profissao;
+    public qualSuaProfissao(): string{
+        return `Sua profissao é ${this.profissao}`
     }
-
-    set profissao(valor: string){
-        this._profissao = valor
-    }
-
 }
 
-const pessoa1 = new Pessoa("Edu", 23);
-console.log(pessoa1);
-console.log(pessoa1.idade);
-
-
-const pessoa2 = new Pessoa("Edu", 23);
-console.log(pessoa2.comer("Macarrao ao molho branco"));
-console.log(pessoa2.fezNiver());
 
 const edu = new Edu("Edu", 23);
 console.log(edu.nome);
---
+++ classes/readonly/index.ts
@@ -0,0 +1,3 @@
+class Pessoa {
+    public readonly nome: string = "Eduardo"
+}
