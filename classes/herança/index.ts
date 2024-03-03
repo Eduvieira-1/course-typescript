588f41edb833f9bae7ac86b8c06de1a7d438afc2
Eduvieira-1 <edu.vieira1@icloud.com>
2024-03-03 17:48:05 -0300

    Inheritance

--- classes/herança/index.ts
+++ classes/herança/index.ts
@@ -17,6 +17,22 @@ class Pessoa{
     }
 }
 
+class Edu extends Pessoa{
+    private _profissao: string = "programador"; 
+    constructor(nome: string, idade: number){
+        super(nome, idade);
+    }
+
+    get profissao(){
+        return this._profissao;
+    }
+
+    set profissao(valor: string){
+        this._profissao = valor
+    }
+
+}
+
 const pessoa1 = new Pessoa("Edu", 23);
 console.log(pessoa1);
 console.log(pessoa1.idade);
@@ -25,3 +41,6 @@ console.log(pessoa1.idade);
 const pessoa2 = new Pessoa("Edu", 23);
 console.log(pessoa2.comer("Macarrao ao molho branco"));
 console.log(pessoa2.fezNiver());
+
+const edu = new Edu("Edu", 23);
+console.log(edu.nome);
